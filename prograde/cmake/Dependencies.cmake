5171de6d38768cbc1c65d10f13d69829b75d1f3e
Florian Cabot <florian.cabot@epfl.ch>
2019-05-20 19:39:57 +0200

    Added Qt Concurrent dependency for texture loading

--- prograde/cmake/Dependencies.cmake
+++ prograde/cmake/Dependencies.cmake
@@ -1,4 +1,6 @@
 find_package(Boost REQUIRED)
+find_package(Qt5 COMPONENTS Concurrent REQUIRED)
 
-set(PROJECT_INCLUDE_DIRS ${Boost_INCLUDE_DIRS})
-set(PROJECT_LIBRARIES ${Boost_LIBRARIES} -lquadmath)
+set(PROJECT_INCLUDE_DIRS ${Boost_INCLUDE_DIRS} ${Qt5Concurrent_INCLUDE_DIRS})
+set(PROJECT_LIBRARIES ${Boost_LIBRARIES} -lquadmath Qt5::Concurrent)
+
